02ffa68be91d0b8078fea97124d64f0b2363c295
Rima Bhavsar <rvbhavsar@yahoo.com>
2022-04-22 21:16:51 +0100

    first commit

---
+++ src/loops/WhileDoWhile.java
@@ -0,0 +1,19 @@
+package loops;
+
+public class While {
+    public static void main(String[] args) {
+        int a=1;
+        int b=10;
+
+        while(a<=10){
+            System.out.println("viva");
+            a++;
+        }
+        do {
+            System.out.println("dwij");
+            b++;
+        }
+            while(b<=10);
+
+        }
+}
